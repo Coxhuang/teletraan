d7eb0fee7dac1902948c838176fbeaf06c61173a
Coxhuang <mhesat@outlook.com>
2020-04-02 00:07:45 +0800

    add vue.js

--- backend/app_ocr/tests.py
+++ backend/app_ocr/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+# 首先安装两个包
+# pip install pytesseract
+# pip install Pillow
+
+from PIL import Image
+import pytesseract
+
+# 指定图片路径和识别的语言
+text = pytesseract.image_to_string(Image.open('/Users/coxhuang/Desktop/1.png'), lang='chi_sim')
+print(text)
